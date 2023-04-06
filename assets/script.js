1822bd6dd527b21bcb50be92e5090c252c6172f8
coc lay <coc.lay97@gmail.com>
2023-04-06 12:16:54 -0400

    Revert "Merge branch 'main' of github.com:coclay1/First-Project-smiles"
    This reverts commit 832bbb12df335dd85e54abed556be719df259aec, reversing
    changes made to 10c82a7a878403d24d3e12d74ea755f8c3a96f85.

--- assets/script.js
+++ assets/script.js
@@ -20,12 +20,6 @@ var buttonClick = function (event) {
     })
     .then(function (data) {
       console.log(data);
-      console.log(data.imdbRating);
-      // grabbing API data here for OMDB
-      var omdbRatingData = (data.imdbRating);
-      omdbRating.textContent = omdbRatingData;
-
-
       var queryURL2 = "https://api.themoviedb.org/3/find/" + data.imdbID + "?api_key=" + tmdbAPIKey + "&language=en-US&external_source=imdb_id";
       fetch(queryURL2)
         .then(function (response) {
@@ -34,10 +28,6 @@ var buttonClick = function (event) {
         })
         .then(function (data) {
           console.log(data);
-          console.log(data.movie_results[0].vote_average);
-          // grabbing API data here for TMDB
-          var tmdbRatingData = (data.movie_results[0].vote_average);
-          tmdbRating.textContent = tmdbRatingData;
 
         })
     })
@@ -53,23 +43,6 @@ var buttonClick = function (event) {
   var movie = movieInput.value;
 
 }
-// store API data, create element,append
-var omdbRating = document.createElement('p');
-var tmdbRating = document.createElement('p');
-
-var omdbLocation = document.querySelector('#omdbRating');
-var tmdbLocation = document.querySelector('#tmdbRating');
-
-omdbLocation.append(omdbRating);
-tmdbLocation.append(tmdbRating);
-
-
-
-
-
-
-
-
 
 
 //Local Storage
@@ -131,4 +104,3 @@ searchHistoryEl.addEventListener("click", function (event) {
   }
 })
 
-init()
